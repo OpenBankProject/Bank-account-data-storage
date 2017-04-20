ae9aff9bd3561bc7a84943b708dd628e2d5d2842
jota <jota@mailbox.tu-berlin.de>
2017-04-20 09:40:52 +0200

    Updated hbci4j

--- src/test/scala/RunWebApp.scala
+++ src/test/scala/RunWebApp.scala
@@ -3,7 +3,7 @@ Open Bank Project - API
 Copyright (C) 2011, 2013, TESOBE / Music Pictures Ltd
 
 This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published by
+it under the terms of the GNU Affero General Public Licenseg as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
